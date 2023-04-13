3f2545c15a7b72b01fe27334ce3973bfba4e6186
Yato <yatoakii@gmail.com>
2023-04-13 22:05:52 +0630

    Can download img now

--- src/Components/UploadSection.js
+++ src/Components/UploadSection.js
@@ -4,6 +4,8 @@ import Example from "../assets/Example.png"
 import front from "../assets/front.png"
 import logOut from "../assets/🦆 icon _log out_.png"
 import back from "../assets/back.png"
+import html2canvas from "html2canvas";
+
 
 const UploadSection = () => {
 
@@ -21,14 +23,24 @@ const UploadSection = () => {
         console.log(formData);
     }
 
+    const handleDownload = () => {
+        const element = document.getElementById("outputImg"); // replace with your div id
+        html2canvas(element, {
+            scale: 25, // Set scale to 25x for full HD resolution (1920x1080)
+            useCORS: true
+        }).then(canvas => {
+            const link = document.createElement("a");
+            link.download = "my-image.png";
+            link.href = canvas.toDataURL();
+            link.click();
+        });
+    }
+
     const editedImg = () => {
-
-
         const overlayImg = {
-            width:"350px",
-            height:"350px",
+            width:"100%",
+            height:"100%",
             position:"absolute",
-            border: "3px solid black",
         }
 
         const mainPic  = {
@@ -39,18 +51,16 @@ const UploadSection = () => {
             position:"absolute"
         }
         const stableImg = {
-            width:"350px",
-            height:"350px"
+            width:"100%",
+            height:"100%"
         }
-
-
 
         return(
             <>
                 
-                <img src={URL.createObjectURL(selectedFile)} alt="" style={mainPic}/>
-                <img src={front} alt="" style={overlayImg}/>
-                <img src={back} alt="" style={stableImg}/>
+                <img src={URL.createObjectURL(selectedFile)} alt="" style={mainPic} id="pic1"/>
+                <img src={front} alt="" style={overlayImg} id="pic2"/>
+                <img src={back} alt="" style={stableImg} id="pic3"/>
             </>
         )
 
@@ -64,18 +74,29 @@ const UploadSection = () => {
         <div className="UploadSection">
             <h2>Transform your photo here!</h2>
             <div className="inputArea">
-                <label htmlFor="imgInput" id="uploadBtn"><img src={logOut} alt="" className="icon"/> Upload Image </label>
+                {selectedFile ? 
+                    <button id="downloadBtn" onClick={handleDownload}><img src={logOut} alt="" className="downloadIcon"/> Download Image</button> : 
+                    <label htmlFor="imgInput" id="uploadBtn"><img src={logOut} alt="" className="icon"/> Upload Image </label>
+                }
+                
                 <input type="file" onChange={onFileChange} accept="image/*" capture="camera" id="imgInput" style={{ display: 'none' }} />
             </div>
             
-            <div className="createImage">
-                {selectedFile ? editedImg() : <img src={Example} alt="" id="exampleImg" />}
+            <div style={{border:"3px solid black"}}>
+                <div className="createImage" id="outputImg">
+                    {selectedFile ? editedImg() : <img src={Example} alt="" id="exampleImg" />}
+                </div>
             </div>
+            
 
-            <div className="control">
-                <button onClick={()=>{setZoom(zoom+4)}}>+</button>
-                <button onClick={()=>{setZoom(zoom-4)}}>-</button>
-            </div>
+            
+
+            {selectedFile && 
+                <div className="control">
+                    <button onClick={()=>{setZoom(zoom+4)}}>+</button>
+                    <button onClick={()=>{setZoom(zoom-4)}}>-</button>
+                </div>
+            }
             
             
             <p>We do not collect your photo.</p>
