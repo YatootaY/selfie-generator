fb63755353b98c537b13763117e1878fcc853ead
Yato <yatoakii@gmail.com>
2023-04-13 16:19:21 +0630

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      Testing
+      <Header/>
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,17 @@
 import React from "react";
+import "./Header.css"
 
 const Header = () => {
 
     return(
-        <div>
-            hi
+        <div className="Header">
+            <h1>Selfie Generator</h1>
+            <ul>
+                <li><a href="#">home</a></li>
+                <li><a href="#transform">transform</a></li>
+                <li><a href="#terms">terms</a></li>
+                <li><a href="#insperation">insperation</a></li>
+            </ul>
         </div>
     )
 }
